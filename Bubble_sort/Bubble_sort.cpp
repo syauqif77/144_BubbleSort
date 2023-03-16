46379a547a0f1bb6bc2427b6eaef0431812ec3df
syauqif77 <syagfm777@gmail.com>
2023-03-16 17:41:43 +0700

    tambahan display

--- Bubble_sort/Bubble_sort.cpp
+++ Bubble_sort/Bubble_sort.cpp
@@ -51,6 +51,17 @@ void unsorted() {
     cout << endl;
 }
 
+void display() {
+    cout << endl;
+    cout << "============================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "============================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << a[j] << endl;
+    }
+    cout << endl;
+}
+
 int main()
 {
 }
