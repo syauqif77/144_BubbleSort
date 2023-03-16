f0fd1bf5a36ab60255822507391d0aa3c716aedd
syauqif77 <syagfm777@gmail.com>
2023-03-16 17:40:24 +0700

    tambahan void bubbleSortArray

--- Bubble_sort/Bubble_sort.cpp
+++ Bubble_sort/Bubble_sort.cpp
@@ -28,6 +28,18 @@ void input() {
     }
 }
 
+void bubbleSortArray() {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
 }
