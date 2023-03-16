7962c3acb62917bf5934fed8b36bf9cd5a5e6c0f
syauqif77 <syagfm777@gmail.com>
2023-03-16 17:41:03 +0700

    tambahan void unsorted

--- Bubble_sort/Bubble_sort.cpp
+++ Bubble_sort/Bubble_sort.cpp
@@ -40,6 +40,17 @@ void bubbleSortArray() {
     }
 }
 
+void unsorted() {
+    cout << endl;
+    cout << "============================" << endl;
+    cout << "Element Array yang belum tersusun" << endl;
+    cout << "============================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << a[j] << endl;
+    }
+    cout << endl;
+}
+
 int main()
 {
 }
